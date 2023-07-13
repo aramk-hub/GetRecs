c99746f625a78e35a22fed9bf19a7e41dc2720f5
Aram Kazorian <aramk@berkeley.edu>
2023-07-13 00:52:39 -0700

    have barebones set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap';
-import {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import Dropdown from './Dropdown';
+import Listbox from './Listbox';
+import Detail from './Detail';
+import { Credentials } from './Credentials';
+import axios from 'axios';
+
+const App = () => {
+
+  const spotify = Credentials();  
+
+  console.log('RENDERING APP.JS');
+
+  const data = [
+    {value: 1, name: 'A'},
+    {value: 2, name: 'B'},
+    {value: 3, name: 'C'},
+  ]; 
+
+  const [token, setToken] = useState('');  
+  const [genresList, setGenresList] = useState({selectedGenre: '', listOfGenresFromAPI: []});
+  const [tracks, setTracks] = useState({selectedTrack: '', listOfTracksFromAPI: []});
+  const [trackDetail, setTrackDetail] = useState(null);
+
+  useEffect(() => {
+
+    axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)      
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+    .then(tokenResponse => {      
+      setToken(tokenResponse.data.access_token);
+
+      axios('https://api.spotify.com/v1/recommendations/available-genre-seeds', {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
+      })
+      .then (genreResponse => {        
+        setGenresList({
+          selectedGenre: genresList.selectedGenre,
+          listOfGenresFromAPI: genreResponse.data.genres.items
+        })
+      });
+      
+    });
+
+  }, [genresList.selectedGenre, spotify.ClientId, spotify.ClientSecret]); 
+
+  const genreChanged = val => {
+    setGenresList({
+      selectedGenre: val, 
+      listOfGenresFromAPI: genresList.listOfGenresFromAPI
+    });
+
+    console.log(val);
+  }
+
+  const buttonClicked = e => {
+    e.preventDefault();
+
+  }
+
+  const listboxClicked = val => {
+
+    const currentTracks = [...tracks.listOfTracksFromAPI];
+
+    const trackInfo = currentTracks.filter(t => t.track.id === val);
+
+    setTrackDetail(trackInfo[0].track);
 
 
-const APIController = (function() {
-		const clientId = '';
-		const clientSecret = '';
 
-		const _getToken = async () => {
-				const result = await fetch('https://accounts.spotify.com/api/token' , {
-						method: 'POST',
-						headers: {
-								'Content Type' : 'application/x-www-form-urlencoded' ,
-								'Authorization' : 'Basic' + btoa(clientId + ':' + clientSecret)
-						},
-						body: 'grant_type=client_credentials'
-				});
-				const data = await result.json();
-				return data.access_token;
-		}
+  }
 
+  
+  
 
-		const _getGenres = async(token) => {
-				const result = await fetch(`https://api.spotify.com/v1/recommendations/available-genre-seeds` , {
-						method: 'GET', 
-						headers: {'Authorization' : 'Bearer' + token}
-				});
-
-				const data = await result.json();
-				return data.genres.items;
-		}
-
-		const _getRecs = async(token, genreId) => {
-
-				const result = await fetch(`https://api.spotify.com/v1/recommendations?seed_genres=${genreId}` , {
-						method: 'GET' , 
-						headers: {'Authorization' : 'Bearer' + token}
-				});
-				
-				const data = await result.json();
-				return data.tracks.items;
-		}
-
-		return {
-				getToken() {
-					return _getToken();
-				},
-				getGenres(token) {
-					return _getGenres();
-				},
-				getRecs(token, genreId) {
-					return _getRecs(token, genreId)
-				}
-		}
-
-})();
-
-
-
-
-function App() {
-	const [searchInput, setSearchInput] = useState("");
-	return (
-		<div className="App">
-			
-		</div>
-	);
+  return (
+    <div className="container">
+      <form onSubmit={buttonClicked}>        
+          <Dropdown label="Genre :" options={genresList.listOfGenresFromAPI} selectedValue={genresList.selectedGenre} changed={genreChanged} />
+          <div className="col-sm-6 row form-group px-0">
+            <button type='submit' className="btn btn-success col-sm-12">
+              Search
+            </button>
+          </div>
+          {/* <div className="row">
+            <Listbox items={tracks.listOfTracksFromAPI} clicked={listboxClicked} />
+            {trackDetail && <Detail {...trackDetail} /> }
+          </div>         */}
+      </form>
+    </div>
+    
+    
+  );
 }
 
 export default App;
---
+++ client/src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '2ba03b82f1d3454184efa1859c5b3c71',
+        ClientSecret: '529cf05446f34c0cb5a94c38953b2ef9'
+    }
+}
+
+export { Credentials };
