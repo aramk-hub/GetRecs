76d86f3eb1f73780ffcb9120c4ddec5158046a99
Aram Kazorian <aramk@berkeley.edu>
2023-07-24 23:44:34 -0700

    input validation, search popover, set up for form submission

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {
-    Alert, 
-    AlertIcon, 
-    AlertTitle,
-    AlertDescription,
-    CloseButton,
+    PopoverArrow,
+    PopoverContent,
+    PopoverCloseButton,
+    PopoverHeader,
+    PopoverBody,
     Center,
     Icon,
     Box,
@@ -38,7 +38,9 @@ import {
     CardBody,
     Stack,
     useDisclosure,
-    Text
+    Text,
+    PopoverTrigger,
+    Popover
   } from '@chakra-ui/react';
 import {
     FiHash,
@@ -91,8 +93,12 @@ const Search = () => {
 
     const validateInputs = (trackList, artistList, genreSeeds) => {
         console.log("VALIDATING INPUTS");
-        if (trackList.length + artistList.length + genreSeeds.length > 5) {
+        if (trackList.length == 0 && artistList.length == 0 && genreSeeds.length == 0) {
             return false;
+        } else if (trackList.length + artistList.length + genreSeeds.length > 5) {
+            return false;
+        } else {
+            return true
         }
     }
 
@@ -103,70 +109,112 @@ const Search = () => {
         var trackList = document.getElementById('trackslist').value.split(',');
         var artistList = document.getElementById('artistlist').value.split(',');
         var genreSeeds = document.getElementById('genrelist').value.split(',');
+        
 
-        // if (!validateInputs(trackList, artistList, genreSeeds)) {
+        trackList = trackList.filter(item => item);
+        artistList = artistList.filter(item => item);
+        genreSeeds = genreSeeds.filter(item => item);
+        
+        if (!validateInputs(trackList, artistList, genreSeeds)) {
+            console.log("ERROR");
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay backdropFilter='blur(10px)'/>
+                <ModalContent>
+                <ModalHeader >Create a Playlist</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody >
+                    This playlist will be added to your spotify account. Let's give it a name and a description!
+                    <FormControl mt={4}>
+                    <FormLabel>Playlist Name</FormLabel>
+                    <Input color="blackAlpha.900" id='playlist_name' />
+                    </FormControl>
 
-        // }
+                    <FormControl mt={4}>
+                    <FormLabel>Description</FormLabel>
+                    <Input color="blackAlpha.900" id='playlist_description' />
+                    </FormControl>
+                </ModalBody>
 
-        // if (!validateInputs(trackList, artistList, genreSeeds)) {
-        //     <alert>Check your inputs</alert>
-        //     return
-        // }
+                <ModalFooter>
+                    <Button whiteSpace="nowrap" textOverflow="ellipsis" overflow="hidden" colorScheme='purple' mr={3} onClick={onClose}>
+                    Close
+                    </Button>
+                    <Button whiteSpace="nowrap" textOverflow="ellipsis" overflow="hidden"   size='md' onClick={createPlaylist}>Create</Button>
+                </ModalFooter>
+                </ModalContent>
+            </Modal>
+        }
+
             
         // Get ArtistIDs
-        
         var artistSeeds = [];
-        for (const artist of artistList) {
-            const{data} = await axios({
-                url: 'https://api.spotify.com/v1/search/',
-                params: {
-                    q: artist,
-                    type: "artist",
-                    limit: 1,
+        console.log(artistList.toString())
+        if (artistList.length > 0) {
+            
+            for (const artist of artistList) {
+                const{data} = await axios({
+                    url: 'https://api.spotify.com/v1/search/',
+                    params: {
+                        q: artist,
+                        type: "artist",
+                        limit: 1,
 
-                },
-                method: 'get',
-                headers: {
-                    Authorization : `Bearer ${token}`
-                }
-            })
-            artistSeeds.push(data.artists.items[0].id);
+                    },
+                    method: 'get',
+                    headers: {
+                        Authorization : `Bearer ${token}`
+                    }
+                })
+                artistSeeds.push(data.artists.items[0].id);
+            }
         }
 
         // Get TrackID
         
         var trackSeeds = [];
-        for (const track of trackList) {
-            const{data} = await axios({
-                url: 'https://api.spotify.com/v1/search/',
-                params: {
-                    q: track,
-                    type: "track",
-                    limit: 1,
+        if (trackList.length > 0) {
+            for (const track of trackList) {
+                const{data} = await axios({
+                    url: 'https://api.spotify.com/v1/search/',
+                    params: {
+                        q: track,
+                        type: "track",
+                        limit: 1,
 
-                },
-                method: 'get',
-                headers: {
-                    Authorization : `Bearer ${token}`
-                }
-            })
-            trackSeeds.push(data.tracks.items[0].id);
+                    },
+                    method: 'get',
+                    headers: {
+                        Authorization : `Bearer ${token}`
+                    }
+                })
+                trackSeeds.push(data.tracks.items[0].id);
+            }
         }
 
         var limit = parseInt(document.getElementById('limit').value);
 
+        const artist_param = artistSeeds.length == 0 ? "" : artistSeeds.toString()
+        const track_param = trackSeeds.length == 0 ? "" : trackSeeds.toString()
+        console.log("artist seeds " + artistSeeds)
+        console.log("artist param: " + artist_param)
+        const params = {
+            seed_artists: artist_param,
+            seed_genres: genreSeeds.toString(),
+            seed_tracks: track_param,
+            limit: limit
+          };
+          
+          for (const key of Object.keys(params)) {
+            if (params[key] === "") {
+              delete params[key];
+            }
+          }
+        
         var recs = [];
+        try {
         const{data} = await axios({
             url: 'https://api.spotify.com/v1/recommendations?',
-            params: {
-                seed_artists: artistSeeds.toString(),
-                seed_genres: genreSeeds.toString(),
-                seed_tracks: trackSeeds.toString(),
-                limit: limit,
-                // target_time_signature: 7,
-                // target_tempo: BPM,
-                // target_key: key
-            },
+            params: params,
             method: 'get',
             headers: {
                 Authorization : `Bearer ${token}`
@@ -176,6 +224,10 @@ const Search = () => {
         setRecs(data.tracks);
         console.log("RECS: " + recs);
         setSearched(true);
+        } catch (error) {
+            console.log("CAUGHT");
+            
+        }
         
     }
 
@@ -223,25 +275,11 @@ const Search = () => {
           }).catch(err => {
               console.error('Request failed', err);
           })
-
-
-
-
-
-
-        //   const pl_id = await (response.json())['id']
-        //   await fetch(`https://api.spotify.com/v1/playlists/${pl_id}/tracks`, {
-        //     method: "POST",
-        //     headers: headers, 
-        //     body: bod
-        //     })
             
-
         setPlaylistCreated(true);
         console.log("GREAT SUCCESS")
-        onClose();      
-              
-                
+        onClose(); 
+
     }
 
     const renderRecs = () => {
@@ -276,7 +314,7 @@ const Search = () => {
             </CardBody>
 
             <Center>
-            <Button mb={3} whiteSpace="nowrap" textOverflow="ellipsis" overflow="hidden" marginTop="20px" width="15" textAlign="center" opacity="1" colorScheme="purple" backgroundColor="purple.500" size='md' onClick={onOpen}>
+            <Button mb={3} whiteSpace="nowrap" textOverflow="ellipsis" overflow="hidden" marginTop="20px" width="15" textAlign="center" colorScheme="purple"  size='md' onClick={onOpen}>
                                     Convert to Playlist
             </Button>
             <Modal isOpen={isOpen} onClose={onClose}>
@@ -322,6 +360,7 @@ const Search = () => {
 
     return (
         
+        
         <div>
             {checkLogin()}
             <div className="search" margin="0 auto" overflow-y="auto" >
@@ -348,7 +387,6 @@ const Search = () => {
                         maxHeight="135%"
                         overflowY='auto' 
                         color="gray.300" 
-                        opacity="80%" 
                         position="absolute" 
                         display="flex"
                         top="5%" 
@@ -405,12 +443,25 @@ const Search = () => {
 
                                         <div className="form-group" >
                                         <FormLabel fontSize= "2vmin" color="blackAlpha.900">Tracks</FormLabel>
-                                        <InputGroup >
-                                        <InputLeftElement pointerEvents='none'>
-                                            <Icon fontSize= "2vmin" float="left" as={FiMusic} color={"gray.500"} />
-                                        </InputLeftElement>
-                                        <Input fontSize= "2vmin" w="90%" color="blackAlpha.900" placeholder='A&W, As It Was' id="trackslist"/>
-                                        </InputGroup>
+                                        <Popover trigger="hover" openDelay={100} closeDelay={100}>
+                                            <PopoverTrigger>
+                                                <InputGroup >
+                                                <InputLeftElement pointerEvents='none'>
+                                                    <Icon fontSize= "2vmin" float="left" as={FiMusic} color={"gray.500"} />
+                                                </InputLeftElement>
+                                                <Input fontSize= "2vmin" w="90%" color="blackAlpha.900" placeholder='A&W, As It Was' id="trackslist"/>
+                                                </InputGroup>
+                                            </PopoverTrigger>
+                                            <PopoverContent w="30vmin" bg="teal.100">
+                                                <PopoverArrow />
+                                                <PopoverCloseButton />
+                                                <PopoverHeader><b>Heads up!</b></PopoverHeader>
+                                                <PopoverBody>If necessary, you should specify the artist of the song
+                                                    just like you would on spotify search (i.e. "Money pink floyd"), since there can be 
+                                                    convoluted results at times.
+                                                </PopoverBody>
+                                            </PopoverContent>
+                                        </Popover>
                                         </div>
 
                                         <div className="form-group" >
@@ -430,7 +481,7 @@ const Search = () => {
                                 
                                 </Stack>
                                 <Center>
-                                <Button whiteSpace="nowrap" textOverflow="ellipsis" overflow="hidden" marginTop="20px" width="15" textAlign="center" opacity="1" colorScheme="purple" backgroundColor="purple.500" size='md' onClick={handleClick}>
+                                <Button whiteSpace="nowrap" textOverflow="ellipsis" overflow="hidden" marginTop="20px" width="15" textAlign="center" colorScheme="purple" size='md' onClick={handleClick}>
                                         Get Recs
                                 </Button>
                                 </Center>
@@ -438,7 +489,13 @@ const Search = () => {
                                 </FormControl>                                
                                 </Fragment>
                             </CardBody>
-                            {renderRecs()}    
+                            {renderRecs()} 
+                            {/* {playlistCreated ? 
+                                <div>
+                                    
+                                </div>
+                             : null} */}
+                             
                     </Card>
                     </Flex>
                 </Flex>
